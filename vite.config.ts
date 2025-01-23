8561a2bf7311072bfd42fca1adc85ec830339477
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2025-01-22 21:29:30 -0500

    Update `base`

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  base: "/mvp-visually-explained/",
 })
